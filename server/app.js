f93bf15c89c93b9e5451442f469948c9c4df41e5
kuiguo1991 <kuiguo1991@gmail.com>
2018-11-28 16:51:42 +0800

    提交2018/11/28#02

--- server/app.js
+++ server/app.js
@@ -16,6 +16,18 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true });
 
 
 const app = express();
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api',hero);
