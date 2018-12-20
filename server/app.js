ee1ae445d2b8ef63a4649be57740862748ac5da6
kuiguo1991 <kuiguo1991@gmail.com>
2018-12-20 13:31:29 +0800

    提交2018/12/20#01

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true });
 const app = express();
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
   if (req.method == 'OPTIONS') {
