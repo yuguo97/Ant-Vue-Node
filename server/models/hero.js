bc10468e136abd338aeb4eff2b3452003e624342
kuiguo <kuiguo1991@gmail.com>
2020-09-02 10:22:12 +0800

    aaa

--- server/models/hero.js
+++ server/models/hero.js
@@ -12,6 +12,7 @@ const heroSchema = mongoose.Schema({
     favourite:String,
     explain:String
 }, { collection: 'myHero'});
+
 //这里mongoose.Schema最好要写上第二个参数，明确指定到数据库中的哪个表取数据，我这里写了myHreo，目的就是为了以后操作数据要去myHreo表中。
 
 
--- server/router/index.js
+++ server/router/index.js
@@ -1,28 +1,28 @@
 
 //引入express模块
-const express = require("express");
+const express = require("express")
 //定义路由级中间件
-const router = express.Router();
+const router = express.Router()
 //引入数据模型模块
 
 
-const hero = require("./../controllers/hreo");
+const hero = require("./../controllers/hreo")
 // 查询所有英雄信息路由
-router.get("/Heros", hero.Heros);
+router.get("/Heros", hero.Heros)
 
 // 通过ObjectId查询单个英雄信息路由
-router.get("/getHero/:id", hero.getHero);
+router.get("/getHero/:id", hero.getHero)
 
 // 添加一个英雄信息路由
-router.post("/addHero", hero.addHero);
+router.post("/addHero", hero.addHero)
 
 //更新一条英雄信息数据路由
-router.put("/putHero/:id",hero.putHero);
+router.put("/putHero/:id",hero.putHero)
 
 // 添加图片路由
-router.post("/addpic/:id", hero.addPic);
+router.post("/addpic/:id", hero.addPic)
 
 //删除一条英雄信息路由
-router.delete("/delHero/:id", hero.delHero);
+router.delete("/delHero/:id", hero.delHero)
 
-module.exports = router;
+module.exports = router
