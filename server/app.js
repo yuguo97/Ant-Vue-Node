c316cd05e25ace6b581ca590fe20d13e6c9a3105
kuiguo <kuiguo1991@gmail.com>
2020-09-02 10:01:27 +0800

    123

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 //定义简单路由
-const express = require('express');
-const app = express();
-const hero = require('./router/index');
-const mongoose = require("mongoose");
+const express = require('express')
+const app = express()
+const hero = require('./router/index')
+const mongoose = require("mongoose")
 
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 
 
 
 
 //这一句是连接上数据库
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true })
 
 //这里的myDbs是数据库的名字，不是表的名字
 
@@ -18,20 +18,20 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true });
 
 
 app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With')
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
 
     if (req.method == 'OPTIONS') {
         res.sendStatus(200);  /让options请求快速返回/
     }
     else {
-    next();
+    next()
     }
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api',hero);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/api',hero)
 app.listen(7551,() => {
     console.log('http://localhost:7551/api')
 });
