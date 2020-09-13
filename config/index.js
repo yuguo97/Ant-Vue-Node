3cfc21b8f2159087a0e69dad8f5d51e110c8f18b
yuguo <admin@192.168.0.114>
2020-09-13 20:36:26 +0800

    修改配置

--- config/index.js
+++ config/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-10 21:30:41
+ * @LastEditTime: 2020-09-13 20:27:12
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /github/element-ui-node/config/index.js
+ */
 'use strict'
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
