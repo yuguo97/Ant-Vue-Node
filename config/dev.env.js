a49c3c977e39fc5af60e6f4be5c95cc860162992
yuguo <admin@192.168.0.114>
2020-09-13 20:59:08 +0800

    配置baseURL

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-10 21:30:41
+ * @LastEditTime: 2020-09-13 20:46:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /github/element-ui-node/config/dev.env.js
+ */
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API:'"./api"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-10 21:30:41
+ * @LastEditTime: 2020-09-13 20:48:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /github/element-ui-node/config/prod.env.js
+ */
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API:'"http://192.168.1.2:8888"'
 }
