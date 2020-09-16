fa14c4d0b552b9ee5a91c703770414de2b61a57c
kuiguo <kuiguo1991@gmail.com>
2020-09-16 17:43:30 +0800

    添加后台接口

--- server/app.js
+++ server/app.js
@@ -1,37 +1,44 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-25 13:57:11
+ * @LastEditTime: 2020-09-16 17:28:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \github\element-ui-node\server\app.js
+ */
 //定义简单路由
-const express = require('express')
-const app = express()
-const hero = require('./router/index')
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+const index = require("./router/index");
+const mongoose = require("mongoose");
 
-const bodyParser = require("body-parser")
-
-
-
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 //这一句是连接上数据库
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true })
+mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
 
 //这里的myDbs是数据库的名字，不是表的名字
 
+app.all("*", function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Content-Length, Authorization, Accept, X-Requested-With"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, GET, DELETE, OPTIONS"
+    );
 
-
-
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With')
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
-
-    if (req.method == 'OPTIONS') {
-        res.sendStatus(200);  /让options请求快速返回/
-    }
-    else {
-    next()
+    if (req.method == "OPTIONS") {
+        res.sendStatus(200);
+        /让options请求快速返回/;
+    } else {
+        next();
     }
 });
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/api',hero)
-app.listen(7551,() => {
-    console.log('http://localhost:7551/api')
+app.use("/api", index);
+app.listen(7551, () => {
+    console.log("http://localhost:7551/api");
 });
---
+++ server/controllers/json.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-16 16:43:36
+ * @LastEditTime: 2020-09-16 17:39:47
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \github\element-ui-node\server\controllers\json.js
+ */
+var path = require("path");
+var fs = require("fs");
+const getJson = function(req, res) {
+    var file = path.join(__dirname, "./../data/bigScreen.json"); //文件路径，__dirname为当前运行js文件的目录
+    //读取json文件
+    fs.readFile(file, "utf-8", function(err, data) {
+        if (!err) {
+            res.send({
+                code: 200,
+                data: JSON.parse(data),
+                message: "success"
+            });
+        } else {
+            res.send({ code: 500, message: "error" });
+        }
+    });
+};
+module.exports = {
+    getJson: getJson
+};
