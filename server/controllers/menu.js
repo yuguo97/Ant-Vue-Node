8116623c55a2fed27fb98bb77106408edbdbb3f7
“kuiguo1991@gmail.com” <kuiguo1991@gmail.com>
2020-09-19 12:05:08 +0800

    添加动态路由

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -1,15 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2020-09-19 11:40:55
- * @LastEditTime: 2020-09-19 11:47:25
+ * @LastEditTime: 2020-09-19 12:04:51
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /github/Ant-Vue-Node/server/controllers/routers.js
  */
 const getMenu = function(req, res) {
+    const data = [
+
+    ]
     res.send({
         code: 200,
-        data: "adsas",
+        data: data,
         message: "success"
     });
 };
