a8503af7786bf751c57431ac452232cd1dc0e6f6
kuiguo1991 <kuiguo1991@gmail.com>
2020-09-20 11:32:19 +0800

    添加api

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-10 21:30:41
- * @LastEditTime: 2020-09-19 09:48:19
+ * @LastEditTime: 2020-09-20 11:12:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /github/element-ui-node/config/dev.env.js
@@ -12,5 +12,5 @@ const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API: '"http://192.168.0.114:8080/api"'
+    API: '"http://192.168.0.106:8080/api"'
 });
--- server/router/index.js
+++ server/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-25 13:57:11
- * @LastEditTime: 2020-09-19 11:46:50
+ * @LastEditTime: 2020-09-20 11:14:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \github\element-ui-node\server\router\index.js
@@ -18,7 +18,7 @@ router.get("/JSON/getJson", Json.getJson);
 router.get("/JSON/getJsonNumber", Json.getJsonNumber);
 
 const Menu = require("../controllers/menu");
-router.get("/MENU/getMenu", Json.getMenu);
+router.get("/MENU/getMenu", Menu.getMenu);
 // const hero = require("./../controllers/hreo")
 // // 查询所有英雄信息路由
 // router.get("/Heros", hero.Heros)
