d127b173b354753453db661f7999822b5d444516
kuiguo <kuiguo1991@gmail.com>
2020-11-12 17:16:27 +0800

    修改接口

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -1,18 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2020-09-19 11:40:55
- * @LastEditTime: 2020-09-20 11:19:58
+ * @LastEditTime: 2020-11-12 17:14:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /github/Ant-Vue-Node/server/controllers/routers.js
  */
 const getMenu = function(req, res) {
-    const data = [
-    ]
+    const data = [];
     res.send({
         code: 200,
         data: data,
-        message: "success"
+        message: "success",
+        time: new Date().toLocaleString()
     });
 };
 
